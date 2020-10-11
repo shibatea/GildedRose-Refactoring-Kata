7d2562b07566eb0964a4c966abbbf2bd99246045
shibatea <ggl.takashi.68@gmail.com>
2020-10-11 18:12:00 +0900

    テストのため、GildedRose のインスタンス変数 Items のアクセス修飾子を public に変更

--- csharp/GildedRose.cs
+++ csharp/GildedRose.cs
@@ -4,10 +4,11 @@ namespace csharp
 {
     public class GildedRose
     {
-        IList<Item> Items;
-        public GildedRose(IList<Item> Items)
+        public readonly IList<Item> Items;
+
+        public GildedRose(IList<Item> items)
         {
-            this.Items = Items;
+            Items = items;
         }
 
         public void UpdateQuality()
