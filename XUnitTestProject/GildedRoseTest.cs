6c25291b883b06387cebfd99fd16364055afe5f2
shibatea <ggl.takashi.68@gmail.com>
2020-10-11 18:12:48 +0900

    Green になるテストを１つ実装する。

--- XUnitTestProject/GildedRoseTest.cs
+++ XUnitTestProject/GildedRoseTest.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using csharp;
 using Xunit;
 
 namespace XUnitTestProject
@@ -8,7 +10,25 @@ namespace XUnitTestProject
         [Fact]
         public void Test1()
         {
+            // Arrange
+            var gildedRose = new GildedRose(new List<Item>
+            {
+                new Item
+                {
+                    Name = "hoge",
+                    Quality = 30,
+                    SellIn = 10
+                }
+            });
 
+            // Act
+            gildedRose.UpdateQuality();
+
+            // Assert
+            var actualItem = gildedRose.Items[0];
+            Assert.Equal("hoge", actualItem.Name);
+            Assert.Equal(29,actualItem.Quality);
+            Assert.Equal(9,actualItem.SellIn);
         }
     }
 }
