45d13fd9ddb552d7b93cfc2d2bc4c7752428fbfa
shibatea <ggl.takashi.68@gmail.com>
2020-10-11 19:25:35 +0900

    test: GildedRose のカバレッジが100%になるようにテストを追加

--- XUnitTestProject/GildedRoseTest.cs
+++ XUnitTestProject/GildedRoseTest.cs
@@ -32,6 +32,30 @@ namespace XUnitTestProject
         }
 
         [Fact]
+        public void ItemName‚ªHoge‚Ìê‡Quality‚ª10()
+        {
+            // Arrange
+            var gildedRose = new GildedRose(new List<Item>
+            {
+                new Item
+                {
+                    Name = "hoge",
+                    Quality = 10,
+                    SellIn = 0
+                }
+            });
+
+            // Act
+            gildedRose.UpdateQuality();
+
+            // Assert
+            var actualItem = gildedRose.Items[0];
+            Assert.Equal("hoge", actualItem.Name);
+            Assert.Equal(8, actualItem.Quality);
+            Assert.Equal(-1, actualItem.SellIn);
+        }
+
+        [Fact]
         public void ItemName‚ªAgedBrie‚Ìê‡()
         {
             // Arrange
