82c99999070bbbd3f8690df030280d7290da22cc
shibatea <ggl.takashi.68@gmail.com>
2020-10-11 18:50:45 +0900

    test: ItemName に応じたテストメソッドを追加

--- XUnitTestProject/GildedRoseTest.cs
+++ XUnitTestProject/GildedRoseTest.cs
@@ -30,5 +30,77 @@ namespace XUnitTestProject
             Assert.Equal(0,actualItem.Quality);
             Assert.Equal(-1,actualItem.SellIn);
         }
+
+        [Fact]
+        public void ItemNameÇ™AgedBrieÇÃèÍçá()
+        {
+            // Arrange
+            var gildedRose = new GildedRose(new List<Item>
+            {
+                new Item
+                {
+                    Name = "Aged Brie",
+                    Quality = 0,
+                    SellIn = 0
+                }
+            });
+
+            // Act
+            gildedRose.UpdateQuality();
+
+            // Assert
+            var actualItem = gildedRose.Items[0];
+            Assert.Equal("Aged Brie", actualItem.Name);
+            Assert.Equal(2, actualItem.Quality);
+            Assert.Equal(-1, actualItem.SellIn);
+        }
+
+        [Fact]
+        public void ItemNameÇ™BackstagePassesÇÃèÍçá()
+        {
+            // Arrange
+            var gildedRose = new GildedRose(new List<Item>
+            {
+                new Item
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    Quality = 0,
+                    SellIn = 0
+                }
+            });
+
+            // Act
+            gildedRose.UpdateQuality();
+
+            // Assert
+            var actualItem = gildedRose.Items[0];
+            Assert.Equal("Backstage passes to a TAFKAL80ETC concert", actualItem.Name);
+            Assert.Equal(0, actualItem.Quality);
+            Assert.Equal(-1, actualItem.SellIn);
+        }
+
+        [Fact]
+        public void ItemNameÇ™SulfurasÇÃèÍçá()
+        {
+            // Arrange
+            var gildedRose = new GildedRose(new List<Item>
+            {
+                new Item
+                {
+                    Name = "Sulfuras, Hand of Ragnaros",
+                    Quality = 0,
+                    SellIn = 0
+                }
+            });
+
+            // Act
+            gildedRose.UpdateQuality();
+
+            // Assert
+            var actualItem = gildedRose.Items[0];
+            Assert.Equal("Sulfuras, Hand of Ragnaros", actualItem.Name);
+            Assert.Equal(0, actualItem.Quality);
+            Assert.Equal(0, actualItem.SellIn);
+        }
     }
 }
